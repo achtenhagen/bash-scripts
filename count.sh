72388d11ce79a2b8ed0db16cd2a519f27e581d96
Maurice Achtenhagen <maurice@achtenhagen.me>
2016-09-21 12:41:55 -0400

    Add first hw script

--- count.sh
+++ count.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo Hello World;
+files=$(ls /bin/)
+declare -a letters=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+
+for l in ${letters[@]} ; do
+    counter=0
+	for i in $files ; do     
+        if [ ${i:0:1} = $l ]; then            
+            counter=$((counter+1))
+        fi
+    done
+    echo $l $counter
+done
